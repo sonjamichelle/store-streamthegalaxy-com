c83e76e5fea0aff7851fa2f0d0004ba7429e6648
Sonja Thomas <sonjamichelle@gmail.com>
2019-06-10 16:41:57 -0500

    Wipe Clean, prep for config
    Signed-off-by: Sonja Thomas <sonjamichelle@gmail.com>

--- admin/includes/classes/currencies.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package admin
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- */
-
-include DIR_FS_CATALOG . DIR_WS_CLASSES . 'currencies.php'; 
