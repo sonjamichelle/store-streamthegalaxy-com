2614840b68fd07ae6bf857eb406d42cd7962efac
Sonja Thomas <sonjamichelle@gmail.com>
2019-06-11 05:38:06 -0500

    Coming Soon Place Holder with Demo Links for Staff
    Signed-off-by: Sonja Thomas <sonjamichelle@gmail.com>

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Untitled 1</title>
+<style type="text/css">
+.auto-style1 {
+	text-align: center;
+}
+.auto-style2 {
+	text-decoration: underline;
+}
+</style>
+</head>
+
+<body>
+
+<p class="auto-style1">
+
+<img alt="Galaxy Streams" height="106" src="Galaxy_New_Logo_trans.png" width="239" />
+
+</p>
+<p class="auto-style1">
+
+The future home of the Galaxy streams web store.</p>
+<p class="auto-style1">
+
+We're working on some ooey gooey goodness so check back soon to see what we've 
+cooked up.</p>
+<hr>
+
+<center>
+
+<table style="width: 50%">
+	<tr>
+		<td colspan="2" align="center">
+		<div id="topbar">
+	<div id="nowplaying">
+				<div id="cc_tunein">
+	<a href="https://centova.streamthegalaxy.com:2199/tunein/luna-stream.pls"><img align="absmiddle" src="https://centova.streamthegalaxy.com:2199/system/images/tunein-pls.png" border="0" alt="Winamp, iTunes" title="Winamp, iTunes" /></a>
+	<a href="https://centova.streamthegalaxy.com:2199/tunein/luna-stream.asx"><img align="absmiddle" src="https://centova.streamthegalaxy.com:2199/system/images/tunein-asx.png" border="0" alt="Windows Media Player" title="Windows Media Player" /></a>
+	<a href="https://centova.streamthegalaxy.com:2199/tunein/luna-stream.ram"><img align="absmiddle" src="https://centova.streamthegalaxy.com:2199/system/images/tunein-ram.png" border="0" alt="Real Player" title="Real Player" /></a>
+	<a href="https://centova.streamthegalaxy.com:2199/tunein/luna-stream.qtl"><img align="absmiddle" src="https://centova.streamthegalaxy.com:2199/system/images/tunein-qtl.png" border="0" alt="QuickTime" title="QuickTime" /></a>
+</div>
+
+
+		<div style="white-space: nowrap; overflow: hidden; margin-right: 8px">
+			Now playing: <a href="/tunein/luna.pls" id="cc_strinfo_summary_luna" class="cc_streaminfo" style="text-overflow: ellipsis">Loading...</a> (<span id="cc_strinfo_server_luna" class="cc_streaminfo"></span>)<br />
+		</div>
+		<script language="javascript" type="text/javascript" src="https://centova.streamthegalaxy.com:2199/system/streaminfo.js"></script>
+
+	</div>
+</div>
+
+		</td>
+	</tr>
+	<tr>
+		<td align="left">
+		
+		<div id="container">
+	
+	<div id="leftnav">
+				
+		<h3>Recent Tracks</h3>
+		
+		<div id="cc_recenttracks_luna" class="cc_recenttracks_list">Loading...</div>
+		
+		<script language="javascript" type="text/javascript" src="https://centova.streamthegalaxy.com:2199/system/recenttracks.js"></script>
+		
+	</div>
+
+		
+		</td>
+		<td align="center">
+		
+<!-- HTML5 PLayer -->
+
+<iframe src="https://player.radioforge.com/v2/shoutcast.html?radiolink=http://192.187.112.242:8000/index.html?sid=1&radiotype=shoutcast&bcolor=&image=https://store.streamthegalaxy.com/Galaxy_New_Logo_trans.png&facebook=https://www.facebook.com&twitter=https://www.twitter.com&title=Galaxy Streams is Streaming the Galaxy&artist=Various" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" style="max-width: 100%;" width="367" height="227"></iframe>
+
+<!-- End HTML5 Player -->
+
+		</td>
+	</tr>
+	<tr>
+	<td colspan="2" align="center">
+	
+	<h3>Request a song</h3>
+	
+		<div class="block">
+			<div class="content">
+			
+				<form>
+				Song artist: <input type="text" id="cc_req_artist_luna" name="request[artist]" size="40" maxlength="127" /><br />
+				Song title: <input type="text" id="cc_req_title_luna" name="request[title]" size="40" maxlength="127" /><br />
+				Dedicated to: <input type="text" id="cc_req_dedi_luna" name="request[dedication]" size="40" maxlength="127" /><br />
+				Your name: <input type="text" id="cc_req_sender_luna" name="request[sender]" size="40" maxlength="127" /><br />
+				Your E-mail: <input type="text" id="cc_req_email_luna" name="request[email]" size="40" maxlength="127" /><br />
+				<input type="button" id="cc_req_button_luna" class="cc_request_form" value="Submit song request" /><br />
+				</form>			
+			</div>
+		</div>
+		<script language="javascript" type="text/javascript" src="https://centova.streamthegalaxy.com:2199/system/request.js"></script>
+		
+	
+	
+	</td>
+	</tr>
+	
+</table>
+
+</center>
+<hr>
+
+<p align="Left" class="auto-style2">
+
+Staff Links:</p>
+<p align="Left">
+
+&nbsp;<a href="https://store.streamthegalaxy.com/CC">Cube Cart Demo</a><br />
+<a href="https://store.streamthegalaxy.com/CC/admin_gs.php">Cube Cart Admin Demo</a></p>
+<p align="Left">
+
+&nbsp;<a href="https://store.streamthegalaxy.com/ZC">Zen Cart Demo</a><br />
+&nbsp;<a href="https://store.streamthegalaxy.com/ZC/poinT-ptg-lUnch">Zen Cart Admin Demo
+</a>
+
+</p>
+
+<hr>
+<p align="right">
+
+<span class="auto-style1">©Copyright 2016-1019</span><br class="auto-style1">
+<span class="auto-style1">Modified - 06/11/2019 </span><br class="auto-style1">
+<span class="auto-style1">Luna Sky - Galaxy Streams</span>
+
+</p>
+
+
+
+</body>
+
+</html>
